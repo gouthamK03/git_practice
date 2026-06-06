ef45bcb0928bdc5af71dfe3ec9e5749c1bfc94b7
K Goutham <kgoutham834@gamil.com>
2026-06-06 21:50:25 +0530

    Added cab-booking feature

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello");
     }
+    public static void bookCab() {
+        System.out.println("Cab booked successfully!");
+    }
 }
