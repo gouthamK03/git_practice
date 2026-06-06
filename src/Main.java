14327bcd4260ed50e28f3430c0b897d54e3908f2
K Goutham <kgoutham834@gmail.com>
2026-06-07 00:35:12 +0530

    Added payment feature

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello");
     }
+    public static void payment(){
+        System.out.println("this is method for payment check");
+    }
 }
